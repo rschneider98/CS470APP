70957b9695dce85fdaac3c42a35bc2764f27d39e
richard <rschneider1st@gmail.com>
2020-10-24 12:47:23 -0500

    finished web layout

--- src/main.py
+++ src/main.py
@@ -7,23 +7,25 @@ from flask import (
     redirect,
     url_for,
     request,
+    make_response,
     render_template
 )
 import mysql              # mysql connector library
 import sqlalchemy as sql  # sqlalchemy is used to connect to db
 import pandas as pd       # used to execute SQL statements on sqlalchemy engine
 import sha3               # cryptographic hash for user verification
+from markupsafe import escape
 
 # SET ENVIRONMENT VARIABLES FOR DB
 # since this will be run at the same location as DB these are formatted for local connections to DB
 host = "127.0.0.1"  # localhost
 port = 3306         # local port
 dbname = "music_library"
-user = os.environ["db_user"]
+dbuser = os.environ["db_user"]
 pwd = os.environ["db_pwd"]     # DO NOT hard-code this
 
 # create SQL engine for DB
-engine = sql.create_engine(f'mysql+mysqlconnector://{user}:{pwd}@{host}:{port}/{dbname}')
+engine = sql.create_engine(f'mysql+mysqlconnector://{dbuser}:{pwd}@{host}:{port}/{dbname}')
 
 # SET UP FLASK APPLICATION
 app = Flask(__name__)
@@ -32,25 +34,42 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # index route for webpage
+    # if cookie does not exist, then the get function returns None
+    user = request.cookies.get('userID')
+    if user is not None:
+        render_template("index.html", user=user)
     return render_template("index.html")
 
 
 @app.route('/about')
 def about():
     # return static information for about page
-    return 'Hello, World!'
+    # check if user is logged in to pass correct variables to template
+    user = request.cookies.get('userID')
+    if user is not None:
+        render_template("about.html", user=user)
+    return render_template("about.html")
 
 
 @app.route('/contact')
 def contact():
-    # return static contact page
-    return 'Hello, World!'
+    # return static contact page, check if user
+    # is logged in to pass correct variables to template
+    user = request.cookies.get('userID')
+    if user is not None:
+        render_template("contact.html", user=user)
+    return render_template("contact.html")
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
+    if request.method == 'POST':
+        # verify that the passwords match
+        # verify that the username and email are unused
+        # update the user information with the new data
+        pass
     # create page to allow users to sign up
-    return 'Hello, World!'
+    return render_template("signup.html")
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -59,7 +78,9 @@ def login():
     if request.method == 'POST':
         # verify user exists and validate password is correct
         # create user authorization cookie
-        user = request.form['nm']
+        if not request.cookies.get('foo'):
+            res = make_response("Setting a cookie")
+            res.set_cookie('foo', 'bar', max_age=60 * 60 * 24 * 365 * 2)
         return redirect(url_for('browse'))
     else:
         return render_template("login.html")
@@ -69,7 +90,7 @@ def login():
 def logout():
     # delete user authorization cookie
     # redirect to index
-    return 'Hello, World!'
+    return redirect(url_for('index'))
 
 
 @app.route('/browse')
@@ -86,16 +107,7 @@ def sitemap():
     return 'Hello, World!'
 
 
-
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
