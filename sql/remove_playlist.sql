739db1ead3c5158d020b3aa5ab765be3531f7504
Richard <rschneider1st@gmail.com>
2020-12-09 02:04:38 -0600

    changed playlist removal into transaction

--- sql/remove_playlist.sql
+++ sql/remove_playlist.sql
@@ -1,7 +1,10 @@
-/* remove a user's playlist */
+/* remove a user's playlist
+   Note: MySQL Server will automatically rollback transaction if error */
 
+START TRANSACTION;
 DELETE FROM playlist
 WHERE playlistID = :playlist_id;
 
 DELETE FROM directory
 WHERE playlistID = :playlist_id;
+COMMIT;
